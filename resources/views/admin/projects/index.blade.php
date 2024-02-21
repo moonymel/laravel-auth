b1b16dc4acbf39b2e4b4f7888a0abe7a1e36023a
moonymel <musumeci.me@gmail.com>
2024-02-21 18:13:30 +0100

    [UPDATE] created index.blade.php for projects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <div class="row my-4">
+        <div class="col-12">
+            qui ci va la tabella con tutti i projects
+        </div>
+    </div>
+</div>
+
+@endsection
